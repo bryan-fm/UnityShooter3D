4094e22d380d6a001b74486684b5961c79295db8
Bryan França <43351790+bryan-fm@users.noreply.github.com>
2023-01-23 21:29:24 -0300

    adicionando pulo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed, gravityModifier, jumpPower;
     public CharacterController charCon;
 
     private Vector3 moveInput;
@@ -14,6 +14,10 @@ public class PlayerController : MonoBehaviour
     public float mouseSensitivity;
     public bool invertX;
     public bool invertY;
+
+    private bool canJump, canDoubleJump;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +31,39 @@ public class PlayerController : MonoBehaviour
         //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
+        //store y velocity
+        float yStore = moveInput.y;
+
         Vector3 vertMove = transform.forward * Input.GetAxisRaw("Vertical");
         Vector3 horiMove = transform.right * Input.GetAxisRaw("Horizontal");
         
         moveInput = horiMove + vertMove;
         moveInput.Normalize();
         moveInput = moveInput * moveSpeed;
+
+        moveInput.y = yStore;
+        moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
+
+        if (charCon.isGrounded) {
+            moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+        }
+
+        //Handle Jumping
+        canJump = Physics.OverlapSphere(groundCheckPoint.position, .25f, whatIsGround).Length > 0;
+        
+        // if (canJump) {
+        //     canDoubleJump = false;
+        // }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump) {
+            moveInput.y = jumpPower;
+
+            canDoubleJump = true;
+        } else if (Input.GetKeyDown(KeyCode.Space) && canDoubleJump) {
+            moveInput.y = jumpPower;
+
+            canDoubleJump = false;
+        }
 
         charCon.Move(moveInput * Time.deltaTime);
 
